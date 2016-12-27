ed140b28ab6ed457dde1a68c9e58b26050a3f00a
Artem Vetkin <Metisav@gmail.com>
2016-12-27 12:04:24 +0500

    bars changed

--- bars.py
+++ bars.py
@@ -2,15 +2,24 @@ import json
 
 
 def load_data(filepath):
-    pass
-
+    data = open(filepath, 'r').read()
+    json_data = json.loads(''.join(data))
+    return json_data
 
 def get_biggest_bar(data):
-    pass
+    max = 0
+    for i in data:
+        if i['SeatsCount'] > max:
+            max = i['SeatsCount']
+    return max
 
 
 def get_smallest_bar(data):
-    pass
+    min = 9999
+    for i in data:
+        if i['SeatsCount']<min:
+            min = i['SeatsCount']
+    return min
 
 
 def get_closest_bar(data, longitude, latitude):
@@ -18,4 +27,6 @@ def get_closest_bar(data, longitude, latitude):
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('data-2897-2016-11-23.json')
+    print(get_biggest_bar(data))
+    print(get_smallest_bar(data))
